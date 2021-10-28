c87c8ba1e8a97a8e2d610e551e94cc75083d2e07
HLN <Huangln555@gmail.com>
2021-10-28 15:05:04 +0800

    First version of Final task, question_template 1 work!!!

---
+++ Final/query.py
@@ -0,0 +1,22 @@
+from py2neo import Graph
+
+from config import Config
+
+
+class Query:
+    def __init__(self):
+        self.graph = Graph(Config.url, user=Config.user, password=Config.password)
+
+    def run(self, cql):
+        result = []
+        find_rela = self.graph.run(cql)
+        for i in find_rela:
+            result.append(i.items()[0][1])
+        return result
+
+
+if __name__ == '__main__':
+    SQL = Query()
+    cql = 'Match (m:Movie) where m.released > 2000 RETURN m limit 5'
+    result=SQL.run(cql)
+    print(result)
